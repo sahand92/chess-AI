695f5cff951d65f326024db81cd79f842918b2a1
sahand92 <sahand92@gmail.com>
2022-05-25 14:04:37 +1000

    add classic evaluator

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import chess
 import chess.svg
 import base64
 import io
+from evaluator import classic_evaluator
 
 board = chess.Board()
 
@@ -10,6 +11,11 @@ board = chess.Board()
 def to_svg(board):
     return chess.svg.board(board)
 
+def legal_moves_str(legal_moves):
+    legal_moves_text = ""
+    for i in range(len(legal_moves)):
+        legal_moves_text += f"{legal_moves[i]},\n"
+    return legal_moves_text
 
 app = Flask(__name__)
 
@@ -17,27 +23,31 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def self_play():
     legal_moves = list(board.legal_moves)
-    legal_moves_str = ""
-    for i in range(len(legal_moves)):
-        legal_moves_str += f"{legal_moves[i]}\n"
+    legal_moves_text = legal_moves_str(legal_moves)
+
     move_text = request.form.getlist('text')
+    move_number = board.fullmove_number
 
     if move_text:
         try:
             move = chess.Move.from_uci(move_text[0])
             if move in legal_moves:
                 board.push(move)
+                legal_moves_text = legal_moves_str(list(board.legal_moves))
             else:
                 move_text = '\'' + move_text[0] + '\'' + ' is an illegal move!'
         except ValueError:
             move_text = '\'' + move_text[0] + '\'' + ' is not a chess move!'
+    
     if request.form.get('reset') == 'reset':
         board.reset()
-    return render_template('index.html', svg_img=Markup(to_svg(board)), text=move_text)
+        legal_moves_text = legal_moves_str(list(board.legal_moves))
+    return render_template('index.html',
+     svg_img=Markup(to_svg(board)), 
+    text=move_text, legal_text=legal_moves_text,
+     evaluation = classic_evaluator(board))
 
 
-
-#    return str(list(board.legal_moves))
 
 if __name__ == '__main__':
     app.run(debug=True)
