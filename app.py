776f8ae7f91c104cab7c32990b3decb3316f38fb
Sahand Assadzadeh <sahand92@gmail.com>
2023-12-22 20:50:25 +1100

    AI move generator

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, Markup, request
+from flask import Flask, render_template, request
 import chess
 import chess.svg
+import random
 import base64
 import io
 from evaluator import classic_evaluator
@@ -12,9 +13,7 @@ def to_svg(board):
     return chess.svg.board(board)
 
 def legal_moves_str(legal_moves):
-    legal_moves_text = ""
-    for i in range(len(legal_moves)):
-        legal_moves_text += f"{legal_moves[i]},\n"
+    legal_moves_text = [f"{x}" for x in legal_moves]
     return legal_moves_text
 
 app = Flask(__name__)
@@ -25,27 +24,33 @@ def self_play():
     legal_moves = list(board.legal_moves)
     legal_moves_text = legal_moves_str(legal_moves)
 
-    move_text = request.form.getlist('text')
+    requested_move = request.form.getlist('requested_move')
+    AI_move = random.choice(legal_moves)
     move_number = board.fullmove_number
 
-    if move_text:
+    if requested_move:
         try:
-            move = chess.Move.from_uci(move_text[0])
+            move = chess.Move.from_uci(requested_move[0])
             if move in legal_moves:
                 board.push(move)
                 legal_moves_text = legal_moves_str(list(board.legal_moves))
             else:
-                move_text = '\'' + move_text[0] + '\'' + ' is an illegal move!'
+                requested_move = '\'' + requested_move[0] + '\'' + ' is an illegal move!'
         except ValueError:
-            move_text = '\'' + move_text[0] + '\'' + ' is not a chess move!'
+            requested_move = '\'' + requested_move[0] + '\'' + ' is not a chess move!'
+
+    if request.form.get('AI move') == 'AI move':
+        board.push(AI_move)
+        legal_moves_text = legal_moves_str(list(board.legal_moves))
     
     if request.form.get('reset') == 'reset':
         board.reset()
         legal_moves_text = legal_moves_str(list(board.legal_moves))
+
     return render_template('index.html',
-     svg_img=Markup(to_svg(board)), 
-    text=move_text, legal_text=legal_moves_text,
-     evaluation = classic_evaluator(board))
+        svg_img=(to_svg(board)), 
+        text=requested_move, legal_text=legal_moves_text,
+        evaluation = classic_evaluator(board))
 
 
 
